e28d505ecb31c555348264f96ab4d8d8a7181bfe
Aditya Mayukh Som <adi.kg2@gmail.com>
2023-05-03 11:33:39 +0530

    socket.io working

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,48 @@
 import Form from "./components/Form";
-import { useState } from "react";
-
+import { useState, useEffect } from "react";
+import { socket } from "./socket";
 function App() {
-  const [chat, setChat] = useState([]);
-  return (
-    <>
-      <div id="message-container">
-        {chat.map((chat, idx) => {
-          return <div key={idx}>{chat}</div>;
-        })}
-      </div>
-      <Form setChat={setChat}></Form>
-    </>
-  );
+    const [chat, setChat] = useState([]);
+    const [isConnected, setIsConnected] = useState(false);
+
+    useEffect(() => {
+        function onConnect() {
+            setIsConnected(true);
+        }
+
+        function onDisconnect() {
+            setIsConnected(false);
+        }
+
+        function handleBroadcastMessage(message) {
+            console.log(message)
+            setChat(chat => [...chat, message.text]);
+        }
+
+
+        socket.on('connect', onConnect);
+        socket.on('disconnect', onDisconnect);
+        socket.on('broadcast-message', handleBroadcastMessage)
+        socket.connect()
+
+        return () => {
+            socket.off('connect', onConnect);
+            socket.off('disconnect', onDisconnect);
+            socket.off('broadcast-message', handleBroadcastMessage)
+            socket.disconnect()
+        };
+    }, []);
+
+    return (
+        <>
+            <div id="message-container">
+                {chat.map((chat, idx) => {
+                    return <div key={idx}>{chat}</div>;
+                })}
+            </div>
+            <Form setChat={setChat}></Form>
+        </>
+    );
 }
 
 export default App;
