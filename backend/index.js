483c5e3bf144565fc54a115d72515ac7f456f7e2
Aditya Mayukh Som <adi.kg2@gmail.com>
2023-05-03 11:39:11 +0530

    socket.io added at backend

--- backend/index.js
+++ backend/index.js
@@ -3,17 +3,21 @@ const express = require('express');
 const bodyParser = require('body-parser')
 const cors = require('cors');
 const dotenv = require('dotenv');
+const http = require('http')
+const { Server } = require('socket.io');
 dotenv.config()
-
+const clientURL = process.env.clientURL
+console.log(clientURL)
 const app = express();
 app.use(express.json())
+
 app.use(cors({
-    origin: 'http://localhost:5173'
+    origin: '*'
 }))
 
 app.get("/", function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/html' })
-    res.end("<h1>this is gg</h1>")
+    console.log(clientURL)
+    res.redirect(301, clientURL)
 
 })
 
@@ -24,13 +28,38 @@ app.post("/get-message", function (req, res) {
     res.json({ msg: rep })
 
 })
-app.listen(6969, function (err) {
+
+const httpServer = http.createServer(app);
+
+const io = new Server(httpServer, {
+    cors: {
+        origin: '*'
+    },
+    connectionStateRecovery: {
+        // the backup duration of the sessions and the packets
+        maxDisconnectionDuration: 2 * 60 * 1000,
+    }
+});
+
+io.on('connection', (socket) => {
+    socket.on("new-message", (message) => {
+        socket.broadcast.emit("broadcast-message", { text: message.text })
+    })
+})
+
+
+
+httpServer.listen(6969, function (err) {
     if (err) {
         console.log("server fucked up")
     } else {
         console.log("server started at port 6969")
     }
 })
+
+
+
+
 
 
 
